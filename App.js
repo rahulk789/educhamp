47d6d942cbe616c838608802170501a50be53a28
samrath-sudesh-acharya <samrathsacharya@gmail.com>
2022-03-12 17:50:00 +0530

    Intial Code Base

---
+++ App.js
@@ -0,0 +1,63 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  Button
+} from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+
+import {
+  Colors,
+} from 'react-native/Libraries/NewAppScreen';
+import HomePage from './screen/HomePage';
+
+
+const App = ()=> {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <NavigationContainer>
+      <HomePage/>
+    </NavigationContainer>
+  );
+};
+
+// const styles = StyleSheet.create({
+//   sectionContainer: {
+//     marginTop: 32,
+//     paddingHorizontal: 24,
+//   },
+//   sectionTitle: {
+//     fontSize: 24,
+//     fontWeight: '600',
+//   },
+//   sectionDescription: {
+//     marginTop: 8,
+//     fontSize: 18,
+//     fontWeight: '400',
+//   },
+//   highlight: {
+//     fontWeight: '700',
+//   },
+// });
+
+export default App;
