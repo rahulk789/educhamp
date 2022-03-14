e645e8ca89099594677cc0bbbf012611f0e58325
samrath-sudesh-acharya <samrathsacharya@gmail.com>
2022-03-14 15:16:36 +0530

    Dummy data for testing

---
+++ dummy/dummy.js
@@ -0,0 +1,32 @@
+export const exam = [
+    {
+        id: 1,
+        name: "Joint Entrance Exam",
+        image: {uri: 'https://uniformapp.in/blog/wp-content/uploads/2019/08/download.jpg'}
+    },
+    {
+        id: 2,
+        name: "Joint Entrance Exam",
+        image: {uri: 'https://uniformapp.in/blog/wp-content/uploads/2019/08/download.jpg'}
+    },
+    {
+        id: 3,
+        name: "Joint Entrance Exam",
+        image: {uri: 'https://uniformapp.in/blog/wp-content/uploads/2019/08/download.jpg'}
+    },
+    {
+        id: 4,
+        name: "Joint Entrance Exam",
+        image: {uri: 'https://uniformapp.in/blog/wp-content/uploads/2019/08/download.jpg'}
+    },
+    {
+        id: 5,
+        name: "Joint Entrance Exam",
+        image: {uri: 'https://uniformapp.in/blog/wp-content/uploads/2019/08/download.jpg'}
+    },
+    {
+        id: 6,
+        name: "Joint Entrance Exam",
+        image: {uri: 'https://uniformapp.in/blog/wp-content/uploads/2019/08/download.jpg'}
+    },
+]
