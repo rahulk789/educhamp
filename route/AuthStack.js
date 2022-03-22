62a148088d36a27edaef479d17d3972cd954fc71
samrath-sudesh-acharya <samrathsacharya@gmail.com>
2022-03-22 15:33:17 +0530

    Merge conflit resovle

--- route/AuthStack.js
+++ route/AuthStack.js
@@ -6,7 +6,6 @@ const Stack = createStackNavigator();
 
 const AuthStack = () => {
     return (
-<<<<<<< HEAD
       <Stack.Navigator>
         <Stack.Screen name="Splash Screen" component={route.SPLASH} options={{headerShown: false}} />
         <Stack.Screen name="Email Screen" component={route.LOGIN} options={{headerShown: false}}/>
@@ -15,13 +14,6 @@ const AuthStack = () => {
         <Stack.Screen name="Sign In Screen" component={route.SIGNIN} options={{headerShown: false}}/>
         <Stack.Screen name = "Details Page" component = {route.DETAILSPAGE} options={{headerShown: false}}/>
         <Stack.Screen name = "Error Screen" component = {route.ERROR} options={{headerShown: false}}/>
-=======
-      <Stack.Navigator screenOption = {{headerShown: false}}>
-        {/* <Stack.Screen name="Splash Screen" component={route.SPLASH} /> */}
-        <Stack.Screen name="Log In Screen" component={route.LOGIN} options={{headerShown: false}}/>
-        <Stack.Screen name="Sign In Screen" component={route.SIGNIN} options={{headerShown: false}} />
-        <Stack.Screen name = "Details Page" component = {route.DETAILSPAGE} options={{headerShown: false}} />
->>>>>>> cd0e2ec198fcc73147c86f0c48cf17c18ffdba4e
       </Stack.Navigator>
     );
   };
